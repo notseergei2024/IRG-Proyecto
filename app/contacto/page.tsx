987e93ef8b78f72fe12c294a4d4b8f4f853fb464
notseergei2024 <jsergiovargas24@gmail.com>
2026-03-19 18:07:02 +0100

    Update page.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,1 +1,13 @@
-
+export default function ContactoPage() {
+  return (
+    <section>
+      <h1>Contacto</h1>
+      <form style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px' }}>
+        <input type="text" placeholder="Tu nombre" />
+        <input type="email" placeholder="Email" />
+        <textarea placeholder="¿En qué podemos ayudarte?"></textarea>
+        <button type="submit">Enviar Mensaje</button>
+      </form>
+    </section>
+  )
+}
