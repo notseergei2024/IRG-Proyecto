c03c71795ea091cda36ded4a74553c68dc0f8e12
notseergei2024 <jsergiovargas24@gmail.com>
2026-03-19 18:11:10 +0100

    Enable React strict mode
    Enable React strict mode in Next.js configuration.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Opciones de configuración si las necesitaras en el futuro
+  reactStrictMode: true,
+}
 
+module.exports = nextConfig
